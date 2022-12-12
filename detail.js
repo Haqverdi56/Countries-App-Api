00d9e202cf132068a34991d8250ee61b281d2d97
Haqverdi56 <qhaqverdi56@gmail.com>
2022-12-12 05:56:10 +0400

    Countires

--- detail.js
+++ detail.js
@@ -1,4 +1,5 @@
 const main = document.getElementById('main')
+let backBTN = document.querySelector(".btn-back")
 
 const url = "https://restcountries.com/v3.1/name/";
 const countryName = localStorage.getItem('countryName')
@@ -16,7 +17,7 @@ getData()
 function fillData(datas) {
     datas.forEach(data=>{
         let borders = data.borders;
-
+        
         main.innerHTML += `
         <div id="detail-flag"><img src="${data.flags.svg}" alt=""></div>
         <div id="detail-info">
@@ -29,44 +30,45 @@ function fillData(datas) {
                     <h4>Sub Region: <span>${data.subregion}</span></h4>
                     <h4>Capital: <span>${data.capital}</span></h4>
                 </div>
-                <div>
+                    <div>
                     <h4>Top Level Domain: <span>be</span></h4>
                     <h4>Currencies: <span>${Object.entries(data.currencies)[0][0]}</span></h4>
                     <h4>Languages: <span>${Object.entries(data.languages)[0]}</span></h4>
                 </div>
             </div>
-            <div>
-                <h4>Border Countries: <span class="borders">France, German, Netherlands</span></h4>
+            <div class="bordersDiv">
+
             </div>
         </div>`
-        
-        // BORDERS
+                
+                // BORDERS  
+                
         let bordersList = "Border Countries:"
+        let btnBorders = document.querySelectorAll(".btn-border");
         if (borders) {
             borders.forEach(element => {
                 bordersList += `
                 <div class="borders-btns">
                     <button class="btn btn-border">${element}</button>
-                </div>
-            `
-        });
-        } else {
-            bordersList += ` This country has no borders`
+                </div>`
+            });
         }
-
-        let btnBorders = document.querySelectorAll(".btn-border");
-        let borderCountry = document.querySelector('.borders')
-            btnBorders.forEach((btnBorder) => {
-                btnBorder.addEventListener("click", function () {
-                    let borderCountry = btnBorder.innerHTML
+        btnBorders.forEach((btnBorder) => {
+            console.log(btnBorder);
+            btnBorder.addEventListener("click", function () {
+                let borderCountry = btnBorder.innerHTML
                     fetch(`https://restcountries.com/v3.1/alpha/${borderCountry}`)
                         .then(res => res.json())
                         .then(data => {
-                            console.log('dat', data);
-                            borderCountry.appenChild()
-                    }
-                    )
+                            console.log(data);
+                            fillData(data[0].name.official)
+                        }
+                        )
             })
+        })
+
+        backBTN.addEventListener("click", function () {
+            window.location = "./index.html"
         })
     });
 }
